6b522ce077f0ff8bcbb5d91859e50ff0bf192b0a
saif <123>
2020-08-19 17:15:34 +0530

    Initial commit

---
+++ app/src/main/java/logic/mania/flavorsdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package logic.mania.flavorsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello No Flavor App");
+    }
+}
---
+++ app/src/paidVersion/java/logic/mania/flavorsdemo/MainActivity2.java
@@ -0,0 +1,19 @@
+package logic.mania.flavorsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello Paid Flavor App");
+    }
+}
